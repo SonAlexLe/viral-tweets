4ad6e504e4f6079e2ef2daaf1c6f7367bb4349f3
Son Le <42708725+SonAlexLe@users.noreply.github.com>
2021-11-03 23:22:11 +0200

    updated training script

--- research/modelling/tweet_torch.py
+++ research/modelling/tweet_torch.py
@@ -1,4 +1,20 @@
-# -*- coding: utf-8 -*-
+import os
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+import torch
+import wandb
+
+import transformers
+from transformers import (
+    AutoTokenizer,
+    AutoModel,
+    Trainer,
+    TrainingArguments,
+    AutoModelForSequenceClassification
+)
 
 # define features in addition to the tweet text
 # set this as an empty list [] to only use the tweet text
@@ -7,33 +23,54 @@ features = [
     'hashtags_count', 'mentions_count',
     'cashtags_count', 'is_quote_tweet'
 ]
-
-import pandas as pd
-import torch
-import numpy as np
-import os
-import transformers
-import wandb
-from torch.utils.data import DataLoader
-from sklearn.model_selection import train_test_split
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModel, Trainer, TrainingArguments
+# features = []
 
 wandb.init(  # addtitional hyperparameters will be defined below
     config={
         "hf_checkpoint": "google/bert_uncased_L-2_H-128_A-2",
         "freeze_bert": False,
         "features": features,
-        "early_stopping_patience": 5,
-        "dataset": "https://users.aalto.fi/les1/tweets_verified.json.xz"
+        "early_stopping_patience": 3,
+        "dropout": 0.2,
+        "dataset": "https://users.aalto.fi/~les1/all_tweets.json.xz"
     },
-    name="test-wandb5",
+    name="savedmodel-all-weighted-tinybert-unfreeze-6vars",
+    group="all-4labels",
     entity="tweet_virality",
+    save_code=True,
     project="ml-dev"
 )
 
+
+training_args = TrainingArguments(
+    report_to="wandb",
+    output_dir=f'./results/{wandb.run.name}',                    # output directory
+    overwrite_output_dir=True,
+    num_train_epochs=20,                       # total number of training epochs
+    per_device_train_batch_size=128,           # batch size per device during training (smaller size for bigger models otherwise out of memory)
+    per_device_eval_batch_size=128,            # batch size for evaluation
+    warmup_steps=1000,                          # number of warmup steps for learning rate scheduler
+    learning_rate=5e-5,
+    weight_decay=1e-2,                         # strength of weight decay
+    logging_steps=50,                          # logs after every x steps
+    evaluation_strategy="epoch",               # when to evaluate
+    save_strategy="epoch",                     # when to save
+    eval_accumulation_steps=10,
+    dataloader_num_workers=os.cpu_count(),  # change this to run faster (might not work on some machines)
+    save_total_limit=1,
+    load_best_model_at_end=True,
+    metric_for_best_model='accuracy',
+    greater_is_better=True                     # for metric_for_best_model
+)
+
+# save whatever you want with this
+code = wandb.Artifact('training_script', type='code')
+code.add_file("tweet_torch.py")
+wandb.run.log_artifact(code)
+
 df = pd.read_json(wandb.config.dataset, lines=True)
 df["label"] = pd.cut(df["retweets_count"], bins=[0,1,10,100,float("inf")], include_lowest=True, right=False)
-df["label"].cat.rename_categories([0,1,2,3], inplace=True)
+df["label"] = df["label"].cat.rename_categories([0,1,2,3])
 
 splits = {}
 checkpoint = wandb.config.hf_checkpoint
@@ -52,9 +89,10 @@ class TweetDataset(torch.utils.data.Dataset):
             features: pd.DataFrame of other features
         """
         self.texts = texts
-        self.features = features.values.astype("int32")
+        self.features = features.values.astype("int32") if features is not None else None
         self.labels = labels
         self.tokenizer = tokenizer
+        self.sample_weights = self.compute_sample_weights(labels)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
@@ -74,6 +112,12 @@ class TweetDataset(torch.utils.data.Dataset):
     def __len__(self):
         return len(self.labels)
 
+    def compute_sample_weights(self, labels):
+        class_sample_counts = np.unique(labels, return_counts=True)[1]
+        class_weights = 1. / class_sample_counts
+        sample_weights = class_weights[labels]
+        return sample_weights
+
 datasets = {}
 for split_name in splits.keys():
     datasets[split_name] = TweetDataset(
@@ -84,12 +128,12 @@ for split_name in splits.keys():
     )
 
 class ViralityClassifier(torch.nn.Module):
-    """Feel free to change the architecture 🤗
+    """Feel free to change the architecture
     This class knows whether there are features besides the tweet body
     (just supply the corresponding n_features)
     https://github.com/The-AI-Summer/Hugging_Face_tutorials/blob/master/vit.py
     """
-    def __init__(self, checkpoint, n_features=0, n_labels=4, freeze_bert=False):
+    def __init__(self, checkpoint, n_features=0, n_labels=4, freeze_bert=False, dropout=0.2):
         super(ViralityClassifier, self).__init__()
         self.bert = AutoModel.from_pretrained(checkpoint)
         self.n_labels = n_labels
@@ -115,8 +159,10 @@ class ViralityClassifier(torch.nn.Module):
         # down to either the number of features
         # or the number of classes if there are no features
         self.bert_processor = torch.nn.Sequential(
-            torch.nn.Dropout(0.2),
+            torch.nn.Dropout(dropout),
             torch.nn.Linear(bert_hdim, bert_hdim//3),
+            # torch.nn.ReLU(),
+            # torch.nn.Linear(bert_hdim//4, bert_hdim//8),
             torch.nn.ReLU(),
             torch.nn.Linear(bert_hdim//3, n_features)
         )
@@ -159,39 +205,19 @@ class ViralityClassifier(torch.nn.Module):
         for param in self.bert.parameters():
             param.requires_grad = False
 
+
 def compute_metric(eval_pred):
     # to be used with the Trainer in the evaluation phase.
     # Takes in a dict (or NamedTuple, not sure). Must return a dict.
-    return {"val_acc": (eval_pred.label_ids == eval_pred.predictions.argmax(-1)).mean()}
+    return {"accuracy": (eval_pred.label_ids == eval_pred.predictions.argmax(-1)).mean()}
 
-training_args = TrainingArguments(
-    report_to="wandb",
-    output_dir='./results',                    # output directory
-    overwrite_output_dir=True,
-    num_train_epochs=3,                        # total number of training epochs
-    per_device_train_batch_size=128,           # batch size per device during training (smaller size for bigger models otherwise out of memory)
-    per_device_eval_batch_size=128,            # batch size for evaluation
-    warmup_steps=500,                          # number of warmup steps for learning rate scheduler
-    learning_rate=1e-4,
-    weight_decay=1e-3,                         # strength of weight decay
-    max_steps=10,                              # comment this out or change this
-    logging_steps=10,                          # logs after every x steps
-    evaluation_strategy="epoch",               # when to evaluate
-    save_strategy="epoch",                     # when to save
-    eval_steps=1000,                           # evaluation is done and logged every eval_steps. not used if evaluation_straty="epoch"
-    save_steps=1000,                           # saves the model every save_steps
-    eval_accumulation_steps=10,
-    dataloader_num_workers=os.cpu_count(),  # change this to run faster (might not work on some machines)
-    load_best_model_at_end=True,
-    metric_for_best_model='accuracy',
-    greater_is_better=True                     # for metric_for_best_model
-)
 
 model = ViralityClassifier(
     checkpoint,
     n_features=len(features),
     n_labels=4,
-    freeze_bert=wandb.config.freeze_bert
+    freeze_bert=wandb.config.freeze_bert,
+    dropout=wandb.config.dropout
 )
 # model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=4)
 
@@ -200,7 +226,27 @@ early_stopping = transformers.EarlyStoppingCallback(
     early_stopping_threshold=0
 )
 
-trainer = Trainer(
+class CustomTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super(CustomTrainer, self).__init__(*args, **kwargs)
+    
+    def get_train_dataloader(self):
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+
+        sample_weights = self.train_dataset.sample_weights
+        sampler = torch.utils.data.WeightedRandomSampler(sample_weights, len(sample_weights))
+        return torch.utils.data.DataLoader(
+            self.train_dataset,
+            batch_size=self.args.train_batch_size,
+            sampler=sampler,
+            collate_fn=self.data_collator,
+            drop_last=self.args.dataloader_drop_last,
+            num_workers=self.args.dataloader_num_workers,
+            pin_memory=self.args.dataloader_pin_memory,
+        )
+
+trainer = CustomTrainer(
     model=model,                         # the instantiated model to be trained
     args=training_args,                  # training arguments, defined above
     train_dataset=datasets["train"],     # training dataset
@@ -213,3 +259,12 @@ trainer = Trainer(
 trainer.evaluate()  # should be no better than 25% accuracy since we have 4 classes
 
 trainer.train()
+
+best_model_artifact = wandb.Artifact(
+    "TinyBertweetClassifier",
+    type="model",
+    description="best TinyBert model for virality prediction"
+)
+best_ckpt = np.min([int(fol.split("-")[1]) for fol in os.listdir(f"./results/{wandb.run.name}")])
+best_model_artifact.add_dir(f"./results/{wandb.run.name}/checkpoint-{best_ckpt}")
+wandb.run.log_artifact(best_model_artifact)
